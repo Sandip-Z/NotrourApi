116eed3fa5a10da6a791e6568a4f133ebf9e0ed9
sandip satyal <sandipsatyal25@gmail.com>
2020-08-24 22:47:13 +0545

    [add] api for POST tour, GET all tours, GET tour by id

--- app.js
+++ app.js
@@ -1,5 +1,51 @@
 const express = require("express");
+const fs = require("fs");
 const app = express();
+
+const tours = JSON.parse(
+  fs.readFileSync(`${__dirname}/data/tours-simple.json`)
+);
+
+app.use(express.json());
+
+app.get("/api/tours", (req, res) => {
+  res.status(200).json({
+    success: true,
+    result: tours.length,
+    data: {
+      tours,
+    },
+  });
+});
+
+app.get("/api/tours/:id", (req, res) => {
+  const id = req.params.id;
+  const result = tours.filter((tour) => tour.id == id);
+
+  res.status(200).json({
+    success: true,
+    data: {
+      tour: result,
+    },
+  });
+});
+
+app.post("/api/tour", (req, res) => {
+  const body = req.body;
+  const newId = tours[tours.length - 1].id + 1;
+  const bodyWithId = { id: newId, ...body };
+  tours.push(bodyWithId);
+  const result = fs.writeFileSync(
+    "./data/tours-simple.json",
+    JSON.stringify(tours)
+  );
+  res.status(201).json({
+    success: true,
+    data: {
+      tour: bodyWithId,
+    },
+  });
+});
 
 app.listen(5000, "localhost", () => {
   console.log("we are listening at 5000...");
