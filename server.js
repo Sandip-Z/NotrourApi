8622cfd5dfd935cefad5e09826c662308ac21bc3
sandip satyal <sandipsatyal25@gmail.com>
2020-09-04 01:08:17 +0545

    [add] mongoose as dependency

--- server.js
+++ server.js
@@ -1,4 +1,27 @@
 const app = require("./app");
+const mongoose = require("mongoose");
+const dotenv = require("dotenv");
+
+dotenv.config({
+  path: "./config.env",
+});
+
+const DB = process.env.DATABASE.replace(
+  "<PASSWORD>",
+  process.env.DATABASE_PASSWORD
+);
+
+mongoose
+  .connect(DB, {
+    useNewUrlParser: true,
+    useFindAndModify: false,
+    useCreateIndex: true,
+    useUnifiedTopology: true,
+  })
+  .then((con) => {
+    // console.log(con.connection);
+    console.log("we are connected to database");
+  });
 
 app.listen(5000, "localhost", () => {
   console.log("we are listening at 5000");
