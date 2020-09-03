a80c01f8ad7db0f545895f42a1634d16b373b352
sandip satyal <sandipsatyal25@gmail.com>
2020-09-04 01:58:08 +0545

    [add] implement CRUD

--- controllers/tourController.js
+++ controllers/tourController.js
@@ -1,107 +1,110 @@
 const fs = require("fs");
+const Tour = require("../models/tourModel");
 
-const tours = JSON.parse(
-  fs.readFileSync(`${__dirname}/../data/tours-simple.json`)
-);
+// const tours = JSON.parse(
+//   fs.readFileSync(`${__dirname}/../data/tours-simple.json`)
+// );
 
 class TourController {
-  getAllTours(req, res) {
-    res.status(200).json({
-      success: true,
-      result: tours.length,
-      data: {
-        tours,
-      },
-    });
+  async getAllTours(req, res) {
+    try {
+      const allTours = await Tour.find();
+      res.status(200).json({
+        success: true,
+        data: allTours,
+      });
+    } catch (e) {
+      res.status(400).json({
+        success: false,
+        message: e,
+      });
+    }
   }
 
-  getTourById(req, res) {
+  async getTourById(req, res) {
     const id = req.params.id;
-    const result = tours.filter((tour) => tour.id == id);
-    res.status(200).json({
-      success: true,
-      requestedAt: req.requestedAt,
-      data: {
-        tour: result,
-      },
-    });
+    try {
+      const tour = await Tour.findById(id);
+      res.status(200).json({
+        success: true,
+        data: tour,
+      });
+    } catch (e) {
+      res.status(400).json({
+        success: false,
+        message: e,
+      });
+    }
   }
 
-  createTour(req, res) {
+  async createTour(req, res) {
+    try {
+      const newTour = await Tour.create(req.body);
+      res.status(201).json({
+        success: true,
+        data: newTour,
+      });
+    } catch (e) {
+      res.status(400).json({
+        success: false,
+        message: e,
+      });
+    }
+  }
+
+  async updateTour(req, res) {
     const body = req.body;
-    const newId = tours[tours.length - 1].id + 1;
-    const bodyWithId = { id: newId, ...body };
-    tours.push(bodyWithId);
-    const result = fs.writeFileSync(
-      `${__dirname}/../data/tours-simple.json`,
-      JSON.stringify(tours)
-    );
-    res.status(201).json({
-      success: true,
-      data: {
-        tour: bodyWithId,
-      },
-    });
+    const id = req.params.id;
+    try {
+      const updatedData = await Tour.findByIdAndUpdate(id, body, {
+        new: true,
+        runValidators: true,
+      });
+      res.status(202).json({
+        success: true,
+        data: updatedData,
+      });
+    } catch (e) {
+      res.status(400).json({
+        success: false,
+        message: e,
+      });
+    }
   }
 
-  updateTour(req, res) {
-    const body = req.body;
+  async deleteTour(req, res) {
     const id = req.params.id;
-    const updated_tours = [...tours];
-    let response = undefined;
-    updated_tours.forEach((tour, index) => {
-      if (tour.id == id) {
-        response = {
-          ...updated_tours[index],
-          ...body,
-        };
-        updated_tours[index] = {
-          ...response,
-        };
-      }
-    });
-    const result = fs.writeFileSync(
-      `${__dirname}/../data/tours-simple.json`,
-      JSON.stringify(updated_tours)
-    );
-    res.status(202).json({
-      success: true,
-      data: {
-        tour: response,
-      },
-    });
-  }
-
-  deleteTour(req, res) {
-    const id = req.params.id;
-    const updated_tours = tours.filter((tour) => tour.id != id);
-    const result = fs.writeFileSync(
-      `${__dirname}/../data/tours-simple.json`,
-      JSON.stringify(updated_tours)
-    );
-    res.status(200).json({
-      success: true,
-      message: `Successfully deleted tour with id ${id}`,
-    });
+    try {
+      const data = await Tour.findByIdAndDelete(id);
+      res.status(204).json({
+        success: true,
+        message: "Successfully deleted data.",
+      });
+    } catch (e) {
+      res.status(400).json({
+        success: false,
+        message: e,
+      });
+    }
   }
 
   checkId(req, res, next, value) {
-    if (!tours.filter((tour) => tour.id == value).length) {
-      return res.status(404).json({
-        success: false,
-        message: `Unable find a tour with id ${value}.`,
-      });
-    }
+    // if (!tours.filter((tour) => tour.id == value).length) {
+    //   return res.status(404).json({
+    //     success: false,
+    //     message: `Unable find a tour with id ${value}.`,
+    //   });
+    // }
     next();
   }
 
   checkBody(req, res, next) {
-    if (!req.body.name || !req.body.price) {
-      return res.status(400).json({
-        success: "false",
-        message: "Price and Name are required field",
-      });
-    }
+    //   if (!req.body.name || !req.body.price) {
+    //     return res.status(400).json({
+    //       success: "false",
+    //       message: "Price and Name are required field",
+    //     });
+    //   }
     next();
   }
 }
