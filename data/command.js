f32c01ccc7b5e30052bc35713c195ff2f3cfb321
sandip satyal <sandipsatyal25@gmail.com>
2020-09-04 16:45:54 +0545

    [add] command line application to import or delete all data from collection

---
+++ data/command.js
@@ -0,0 +1,45 @@
+const mongoose = require("mongoose");
+const dotenv = require("dotenv");
+const fs = require("fs");
+const Tour = require("../models/tourModel");
+
+dotenv.config({
+  path: "../config.env",
+});
+
+const DB = process.env.DATABASE.replace(
+  "<PASSWORD>",
+  process.env.DATABASE_PASSWORD
+);
+
+mongoose
+  .connect(DB, {
+    useNewUrlParser: true,
+    useFindAndModify: false,
+    useCreateIndex: true,
+    useUnifiedTopology: true,
+  })
+  .then((con) => {
+    // console.log(con.connection);
+    console.log("we are connected to database");
+  });
+
+const data = JSON.parse(
+  fs.readFileSync(`${__dirname}/tours-simple.json`, "utf-8")
+);
+
+const importData = async () => {
+  await Tour.create(data);
+  console.log("Created Successfully");
+};
+
+const deleteData = async () => {
+  await Tour.deleteMany();
+  console.log("Deleted Successfully");
+};
+
+if (process.argv[2] === "--import") {
+  importData();
+} else if (process.argv[2] === "--delete") {
+  deleteData();
+}
